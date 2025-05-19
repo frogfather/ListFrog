c28bec3d44d82c34d8d6b75096cdddab57de5730
John Campbell <j.a.campbell@icloud.com>
2025-05-19 13:43:13 +0100

    initial project

---
+++ ListFrog/ListFrogApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListFrogApp.swift
+//  ListFrog
+//
+//  Created by John Campbell on 19/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct ListFrogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ListFrogTests/ListFrogTests.swift
@@ -0,0 +1,17 @@
+//
+//  ListFrogTests.swift
+//  ListFrogTests
+//
+//  Created by John Campbell on 19/05/2025.
+//
+
+import Testing
+@testable import ListFrog
+
+struct ListFrogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
